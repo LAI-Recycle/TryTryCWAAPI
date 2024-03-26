a25bec5a30758d1eae85f616006c9ba80fc1ec46
LAI <77723979+LAI-Recycle@users.noreply.github.com>
2024-03-26 18:08:06 +0800

    20240326 1807

---
+++ 20240300TryCWAAPI/Models/MongoDBSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _20240300TryCWAAPI.Models
+{
+    public class MongoDBSettings
+    {
+        public string ConnectionURL { get; set; } = null;
+        public string DatabaseName { get; set; } = null;
+        public string CollectionName { get; set; } = null;
+
+
+    }
+}
